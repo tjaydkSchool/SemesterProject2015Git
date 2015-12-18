527466e2e1aef5f4633c3748d57a3fb4d4ed713a
andreassnorrason <Asnorrason@Andreass-MacBook-Pro.local>
2015-12-18 04:08:26 +0100

    changed login restriction

--- semesterSeedFall2015-master/AngSeedClient/app/view6/view6.js
+++ semesterSeedFall2015-master/AngSeedClient/app/view6/view6.js
@@ -20,30 +20,31 @@ angular.module('myApp.view6', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
                     self.airline = $rootScope.airlineNameRoot;
                     self.fromDestS = $rootScope.fromDest;
                     self.toDestS = $rootScope.toDest;
-                    
+
                     self.reserveTryVariable = ({
                         "numberOfSeats": $rootScope.passengersCounter, //tages fra søgningen
                         "ReserveeName": self.reserveeName, //Skrives ved reservering
                         "ReservePhone": self.reserveePhone, //skrives ved reservering
-                        "ReserveeEmail": "test@test.dk", //tages fra user login
+                        "ReserveeEmail": $rootScope.username, //tages fra user login
                         "flightID": self.flightD, // tages fra ui-accordion information
                         "Passengers": $rootScope.passengersCount});
                     self.reserveFunctionAsJSON()
                 };
 
                 self.reserveFunctionAsJSON = function () {
-                    if($scope.isAdmin === true || $scope.isUser === true){
-                    var res = $http.post('api/flightreservation/' + self.airline + "/"+ self.fromDestS +"/"+ self.toDestS, self.reserveTryVariable); //arline bliver taget fra uib-accorsdion information
-                    res.success(function (data, status, headers, config) {
-                        self.message = data;
-                        alert("Reservation completed");
-                    });
-                    res.error(function (data, status, headers, config) {
-                    });
-                }else{
-                    alert("You have to login");
+                    if ($rootScope.username === "" || $rootScope.username === null) {
+                        var res = $http.post('api/flightreservation/' + self.airline + "/" + self.fromDestS + "/" + self.toDestS, self.reserveTryVariable); //arline bliver taget fra uib-accorsdion information
+                        res.success(function (data, status, headers, config) {
+                            self.message = data;
+                            alert("Reservation completed");
+                        });
+                        res.error(function (data, status, headers, config) {
+                        });
+                    } else {
+                        alert("You have to login");
+                    }
+                    ;
                 };
-            };
             }]);
 
 
